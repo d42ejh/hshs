86f3d603fb580b7fab845837e23d4b5c58e892d2
abc <tq9fvi8xg1iz@tutanota.com>
2022-01-26 07:40:26 -0500

    wip example

--- examples/practical_example.rs
+++ examples/practical_example.rs
@@ -5,9 +5,12 @@ use openssl::rsa::Rsa;
 use openssl::sign;
 use openssl::sign::{Signer, Verifier};
 
+struct Message {}
+
 //openssl doc: https://docs.rs/openssl/latest/openssl/sign/index.html
+// cargo run --example practical_example
 fn main() {
-    // generate an new challenge
+    //X generates an new challenge
     let version = 1;
     let bits = 10;
     let challenge = H::new(version, bits);
@@ -18,8 +21,21 @@ fn main() {
     let serialized_challenge = challenge.to_bytes();
 
     //sign the (serialized) challenge
-    let mut signer = Signer::new(MessageDigest::sha3_512(), &keypair).unwrap();
-    signer.update(&serialized_challenge).unwrap();
-    let signature = signer.sign_to_vec().unwrap();
-    //
+    // let mut signer = Signer::new(MessageDigest::sha3_512(), &keypair).unwrap();
+    // signer.update(&serialized_challenge).unwrap();
+    // let signature = signer.sign_to_vec().unwrap();
+
+    //Y receives the challenge from X(typically via network)
+    let mut received_challenge = H::from_bytes(&serialized_challenge);
+    // solve the challenge
+    assert!(received_challenge.solve(None));
+    assert!(received_challenge.verify());
+    let serialized_solved_challenge = received_challenge.to_bytes();
+
+    // X receives the solved challenge from Y
+    let received_challenge = H::from_bytes(&serialized_solved_challenge);
+    if received_challenge.verify() {
+        //do something
+    }
+    
 }
